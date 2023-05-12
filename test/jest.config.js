ea7c39e1ab9408cc27b58a18ec148af1ab414404
joaodutra88 <jvitordutra@gmail.com>
2023-05-12 20:44:14 -0300

    aumentando testTimeout

--- test/jest.config.js
+++ test/jest.config.js
@@ -5,3 +5,8 @@ module.exports = {
    coverageReporters: ["text", "html"],
    coverageDirectory: "coverage",
 };
+
+// jest.config.js
+module.exports = {
+   testTimeout: 10000, // Limite de tempo de 10 segundos para todos os testes
+};
